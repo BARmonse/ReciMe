fe2c6542ee8f01451d224ae72c668f4de2acd6d7
Bruno Rius <brunorius3@gmail.com>
2025-06-20 23:57:46 -0300

    First commit

---
+++ src/main/java/TechnicalAssignment/ReciMe/ReciMeApplication.java
@@ -0,0 +1,13 @@
+package TechnicalAssignment.ReciMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TechnicalAssignment/ReciMe/ReciMeApplicationTests.java
@@ -0,0 +1,13 @@
+package TechnicalAssignment.ReciMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
